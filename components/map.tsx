697a11c7e3b53f556387eebff47608c93e70dbad
selimdilsad <selimdilsadercan@gmail.com>
2023-10-11 15:04:01 +0300

    fixed: @types/leaftlet issue

--- components/map.tsx
+++ components/map.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-// @ts-ignore
 import L from "leaflet";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 
@@ -17,20 +16,20 @@ L.Icon.Default.mergeOptions({
   shadowUrl: markerShadow.src
 });
 
-interface Props {
+interface MapProps {
   center?: number[];
 }
 
 const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-function Map({ center }: Props) {
+const Map: React.FC<MapProps> = ({ center }) => {
   return (
     <MapContainer center={(center as L.LatLngExpression) || [51, -0.09]} zoom={center ? 4 : 2} scrollWheelZoom={false} className="h-[35vh] rounded-lg">
       <TileLayer url={url} attribution={attribution} />
       {center && <Marker position={center as L.LatLngExpression} />}
     </MapContainer>
   );
-}
+};
 
 export default Map;
